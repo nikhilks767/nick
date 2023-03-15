2696e9f5fbce63af93947626abef9313947ac23c
nikhilks767git config --global user.email nikhilks767@gmail.com <nikhilks767@gmail.com>
2023-03-15 15:52:29 +0530

    tenth commit

---
+++ prg10.sh
@@ -0,0 +1,15 @@
+echo "Enter a number :"
+read n
+rev=0
+while [ $n -gt 0 ]
+do
+d=`expr $n % 10`
+if [$d -eq 0 -n $rev -eq 0]
+then
+printf "$d"
+fi
+rev=`expr $rev \* 10 + $d`
+n=`expr $n / 10`
+done
+echo "Reverse = $rev"
+
